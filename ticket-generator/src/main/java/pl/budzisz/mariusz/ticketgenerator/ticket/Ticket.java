b939b1b3afcc9250d0b791cb858ce284c551ee6d
Mariusz Budzisz <mariusz.budzisz@yahoo.com>
2019-05-01 19:15:48 +0200

    Generating Ticket Class 2

---
+++ ticket-generator/src/main/java/pl/budzisz/mariusz/ticketgenerator/ticket/Ticket.java
@@ -0,0 +1,36 @@
+package pl.budzisz.mariusz.ticketgenerator.ticket;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+
+import pl.budzisz.mariusz.ticketgenerator.parking.Slot;
+
+@Scope("prototype")
+@Component
+public class Ticket {
+	
+	int number = 1;
+	
+	@Autowired
+	Slot slot;
+	
+	public void setNumber(int number) {
+		this.number = number;
+	}
+	
+	public void occupySlot(Slot slot) {
+		this.slot = slot;
+		slot.setOccupied(true);
+	}
+	
+	public Ticket() {
+		this.number ++;
+	}
+
+	@Override
+	public String toString() {
+		return "Ticket [NO. " + number + ", Parking Lot =" + slot + "]";
+	}
+	
+}
