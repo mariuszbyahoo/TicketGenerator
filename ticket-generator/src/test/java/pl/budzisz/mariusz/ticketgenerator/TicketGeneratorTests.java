f334d50beafc3c7d8069ca2977ac3e43c93ca14d
Mariusz Budzisz <mariusz.budzisz@yahoo.com>
2019-05-01 21:45:07 +0200

    Adding a unit test to check is Ticket's Scope working propperly

---
+++ ticket-generator/src/test/java/pl/budzisz/mariusz/ticketgenerator/TicketGeneratorTests.java
@@ -0,0 +1,36 @@
+package pl.budzisz.mariusz.ticketgenerator;
+
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import pl.budzisz.mariusz.ticketgenerator.parking.ParkingController;
+import pl.budzisz.mariusz.ticketgenerator.ticket.Ticket;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TicketGeneratorTests {
+	
+	@Autowired
+	ParkingController parkingController;
+
+	@Test
+	public void contextLoads() {
+	}
+	
+	@Test
+	public void TicketGeneration() {
+		for(int i = 0 ; i < 10 ; i ++) {
+			parkingController.ticketInfo();
+		}
+		if(Ticket.number == 10) {
+			assertTrue(true);
+		}
+	}
+
+}
