28aa4cc1badc4c916982febf1405b64c6c1858b0
Mariusz Budzisz <mariusz.budzisz@yahoo.com>
2019-05-04 22:01:19 +0200

    Adding an IText 7 lib usage and making possibility of creating a basic PDF files with content

--- ticket-generator/src/main/java/pl/budzisz/mariusz/ticketgenerator/parking/ParkingController.java
+++ ticket-generator/src/main/java/pl/budzisz/mariusz/ticketgenerator/parking/ParkingController.java
@@ -1,16 +1,20 @@
 package pl.budzisz.mariusz.ticketgenerator.parking;
 
+import java.io.File;
+import java.io.IOException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
-
 import pl.budzisz.mariusz.ticketgenerator.ticket.Ticket;
 
 @RestController
 public class ParkingController {
+	
+	public static final String DEST = "docs/parkingFile.pdf"; 
 
     @Autowired
     SlotService service;
@@ -30,6 +34,13 @@ public class ParkingController {
     @ResponseBody
     public String parkingInfo (){
         return String.format(service.writeParkingInfo());
+    }
+    
+    @RequestMapping(path = "/parkingPdf", method = RequestMethod.GET)
+    public void writeFile() throws IOException{
+    	File file = new File(DEST);
+    	file.getParentFile().mkdirs();
+    	service.createPdf(DEST);
     }
     
     @PutMapping("/getTicket")
--- ticket-generator/src/main/java/pl/budzisz/mariusz/ticketgenerator/parking/SlotService.java
+++ ticket-generator/src/main/java/pl/budzisz/mariusz/ticketgenerator/parking/SlotService.java
@@ -1,10 +1,17 @@
 package pl.budzisz.mariusz.ticketgenerator.parking;
 
-import java.util.ArrayList;
-import java.util.List;
-
+import java.io.File;
+import java.io.IOException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.itextpdf.kernel.font.PdfFont;
+import com.itextpdf.kernel.font.PdfFontFactory;
+import com.itextpdf.kernel.pdf.PdfDocument;
+import com.itextpdf.kernel.pdf.PdfWriter;
+import com.itextpdf.layout.Document;
+import com.itextpdf.layout.element.Paragraph;
+import com.itextpdf.layout.element.Text;
 
 @Service
 public class SlotService{
@@ -25,5 +32,21 @@ Parking parking;
     	System.out.println(parking.getRows().
     			get(columnNumber).getSlotList().
     			get(slotNumber).toString());
+    }
+    
+    public void createPdf(String dest) throws IOException{
+    	
+    	PdfDocument pdf = new PdfDocument(new PdfWriter(dest));
+    	
+    	Document document = new Document(pdf);
+    	
+    	PdfFont font = PdfFontFactory.createFont();
+    	
+    	Text title = new Text("Tytul");
+    	Text author = new Text("Autor");
+    	Paragraph p = new Paragraph().add(title).add(" Napisane przez ").add(author);
+    	document.add(p);
+    	document.close();
+    	
     }
 }
